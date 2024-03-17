0c731e176883f164e3d15087b00435240445709b
MrDemonid <MrDemonid@yandex.ru>
2024-03-17 18:43:36 +0300

    add infantry action

--- src/person/InfantryBase.java
+++ src/person/InfantryBase.java
@@ -31,9 +31,69 @@ public abstract class InfantryBase extends PersonBase {
         level = 1;
     }
 
+    private void move(PersonBase target)
+    {
+        float vx = target.position.getX() - position.getX();
+        float vy = target.position.getY() - position.getY();
+        float len = (float) Math.sqrt(vx*vx + vy*vy);
+        if (len != 0f)
+        {
+            len = 1.0f / len;
+            vx *= len;
+            vy *= len;
+        }
+        int nx = (int) (position.getX() + (vx + 0.5f));
+        int ny = (int) (position.getY() + (vy + 0.5f));
+        System.out.println(name + ": перемещается на (" + nx + ", " + ny + ")");
+        position.moveTo(nx, ny);
+    }
+
+    private void kick(PersonBase target, boolean isMoved)
+    {
+        System.out.print(name + ": бьёт " + target);
+        int damage = getRound(power, 10) + (power / 10) * level;
+        boolean critical = (this.agility/3) >= rnd.nextInt(100);
+        if (critical)
+        {
+            damage *= 2.0f;
+        }
+        int res = target.getDamage(damage);
+        if (res > 0)
+        {
+            if (critical)
+                System.out.print(" и наносит критический удар в " + res + " повреждений!");
+            else
+                System.out.print(" и наносит " + res + " повреждений.");
+        } else {
+            System.out.print(", но " + target.name + " увернулся!");
+        }
+        if (target.health <= 0)
+        {
+            System.out.print("\n" + target + " вышел из чата!");
+        }
+    }
 
     @Override
     public void step(ArrayList<PersonBase> enemies) {
+        if (health <= 0)
+            return;
+        PersonBase target = this.findNearestPerson(enemies);
+        if (target!= null)
+        {
+            float dist = position.distanceTo(target.position);
+            if (dist <= 1.0f)
+            {
+                // бьём
+                kick(target, false);
+            } else {
+                move(target);
+                if (position.distanceTo(target.position) <= 1.0f)
+                {
+                    // бьём с ходу, с меньшей силой
+                    kick(target, true);
+                }
+            }
+        }
 
     }
 
